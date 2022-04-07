d3cddb104d1f2b12551f7f2f9f0aabd58a5df2c6
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-07 23:34:22 +0800

    Test

--- Ultimate A^X.lua
+++ Ultimate A^X.lua
@@ -4,8 +4,6 @@ local w = library:CreateWindow("A") -- Creates the window
 
 local b = w:CreateFolder("B") -- Creates the folder(U will put here your buttons,etc)
 
-local numero = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100
-
 b:Label("TNightmareHA GUI",{
     TextSize = 25; -- Self Explaining
     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
@@ -27,7 +25,7 @@ b:Slider("Walkspeed",{
     max = 100;
     precise = true; -- max 2 decimals
 },function(value)
-    numero
+    print(value)
 end)
 
 b:Dropdown("Dropdown",{"A","B","C"},true,function(mob) --true/false, replaces the current title "Dropdown" with the option that t
