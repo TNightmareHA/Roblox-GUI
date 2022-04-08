dd21f65a5e8fa6c22b663d5c7e79062778950a2b
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-08 12:17:59 +0800

    Update Ultimate-Arceus-X.lua

--- Ultimate-Arceus-X.lua
+++ Ultimate-Arceus-X.lua
@@ -1,10 +1,17 @@
-wait(1.5)
-game.StarterGui:SetCore("SendNotification", {
+wait(2)
+game.StarterGui:SetCore("SendNotification",{
 Title = "Ultimate A^X GUI";
-Text = "";
-Icon = "rbxassetid://57254792"; -- the image if u want. 
-Duration = 10;
+Text = "Сделан TNightmareHA A^X & TNHA";
+Icon = "rbxassetid://9307520762";
+Duration = 5;
 })
+
+game.StarterGui:SetCore("SendNotification",{
+Text = "БОЛЬШОЕ СПАСИБО ЗА ПОДДЕРЖКУ МЕНЯ";
+Duration = 5;
+})
+
+local Edition = "Coming Soon"
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
 
@@ -12,19 +19,30 @@ local w = library:CreateWindow("Обычный эксплойт")
 local Collide = w:CreateFolder("Гуманоидный подвиг")
 local Multiverse = w:CreateFolder("Игровой эксплойт GUI")
 
-Collide:Label("GUI",{
+Collide:Label("Нормальный",{
     TextSize = 25;
     TextColor = Color3.fromRGB(255,255,255);
     BgColor = Color3.fromRGB(69,69,69);
     
-}) 
+})
 
 Collide:Button("IY_FE",function()
     defaultsettings = { prefix = '!'; StayOpen = false; keepIY = true; logsEnabled = false; jLogsEnabled = false; aliases = {}; binds = {}; WayPoints = {}; PluginsTable = {}; } defaults = game:GetService("HttpService"):JSONEncode(defaultsettings) writefile("IY_FE.iy",defaults) loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
-Collide:Button("Fly GUI",function()
+Collide:Button("Fly UI",function()
     loadstring(game:HttpGet("https://pastebin.com/raw/PztijdCc"))()
+end)
+
+Collide:Button("Keyboard UI",function()
+    --This is only for Arceus X or whatever Mobile Android devices you're using.
+    --Note: If you see or heard about iOS, well they might tricking you! Do not fall for their troll, there's nothing Exploit for iOS (iPhone/iPad) Devices!
+
+    --Do not copy the loadstring below. Proceed to the Raw version of pastebin.
+    --For an example: loadstring(game:HttpGet("https://pastebin.com/raw/code", true))() . So this is must be true! Then for the confusing part is why i put a "code" on >>/raw/code"<<? Because pastebin has a lot of creation random conditional code!
+
+    --More example: https:pastebin.com/raw/RRq092XT . Remember that the code has 8 digits!
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/manimcool21/Keyboard-FE/main/Protected%20(3).lua"))()
 end)
 
 Collide:Slider("Walkspeed",{
@@ -41,6 +59,21 @@ Collide:ColorPicker("ColorPicker",Color3.fromRGB(255,0,0),function(color)
     print(color.." <-- Thanks for using with Color")
 end)
 
+Multiverse:Label("Игры",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(69,69,69);
+    
+})
+
 Multiverse:Button("Specter GUI",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DaC9489/DaHub/main/specter",true))()
+end)
+
+Multiverse:Button("Pet Simulator X",function()
+    print(Edition.." - Please wait for the updates!")
+end)
+
+Multiverse:Button("Ragdoll Engine",function()
+    
 end)
