74b7dc3e15e1d0411fe2c69e214a2ba3a15d02bc
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-09 22:43:02 +0800

    Create WeaponMasters.lua

---
+++ WeaponMasters.lua
@@ -0,0 +1,10 @@
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/eWKgbdix", true))()
+
+local window = library:CreateWindow('Weapon Masters V0.1')
+window:Section('Auto Farm')
+
+local tab = window:Toggle('Swing', {flag = "toggle1"})
+local tab = window:Toggle('Rebirth', {flag = "toggle1"})
+local tab = window:Toggle('Prestige', {flag = "toggle1"})
+
+window:Section('Coming Soon')
