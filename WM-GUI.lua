cc44bbb8ec92fad7696a8c32a7426eabe7feba9c
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-09 21:59:23 +0800

    Update WM-GUI.lua

--- WM-GUI.lua
+++ WM-GUI.lua
@@ -1,32 +1,30 @@
-local library = loadstring(game:HttpGet(("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua")))()
-
-local w = library.CreateLib("Weapon Masters V0.1", "DarkTheme")
-local a = w:NewTab("Game")
-local b = a:NewSection("Auto Farm")
-
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Weapon Masters V0.1", "Synapse")
+local Tab = Window:NewTab("Auto Farm")
+local Section = Tab:NewSection("Game")
 
 
-b:NewToggle("Auto Click", "Makes the game to Click for you!", function(state)
-    if state then
-        --[[game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.SwordService.Swing:FireServer()]]print("Boom")
+
+Section:NewToggle("Auto Click", "Makes the game to Click for you!", function(bool)
+    if bool then
+        game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.SwordService.Swing:FireServer()
     else
         print("AutoSwing has been turned off")
     end
 end)
 
-b:NewToggle("Auto Rebirth", "Makes the game to Auto Rebirth for you!", function(state)
-    if state then
-        --[[game.GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.Rebirth:InvokeServer(unpack(args))]]print("Boom")
+Section:NewToggle("Auto Rebirth", "Makes the game to Auto Rebirth for you!", function(bool)
+    if bool then
+        game.GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.Rebirth:InvokeServer(unpack(args))
     else
         print("AutoRebirth has been turned off")
     end
 end)
 
-b:NewToggle("Auto Prestige", "Makes the game to Auto Prestige for you!", function(state)
-    if state then
-        --[[game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.PrestigeService.Prestige:InvokeServer()]]print("Boom")
+Section:NewToggle("Auto Prestige", "Makes the game to Auto Prestige for you!", function(bool)
+    if bool then
+        game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.PrestigeService.Prestige:InvokeServer()
     else
         print("AutoPrestige has been turned off")
     end
 end)
-
