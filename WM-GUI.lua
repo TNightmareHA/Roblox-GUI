0b3057278db26f86d6c832e7e8f85f0403d0b826
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-09 21:18:39 +0800

    Update WM-GUI.lua

--- WM-GUI.lua
+++ WM-GUI.lua
@@ -1,16 +1,13 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local library= loadstring(game:HttpGet(("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua")))()
 
-local Window = Library.CreateLib("Weapon Masters V0.1", "BloodTheme")
+local w = library.CreateLib("Weapon Masters V0.1", "BloodTheme")
 
-local Tab = Window:NewTab("Main")
-
-local Tab1 = Window:NewTab("Game")
-local Section1 = Tab1:NewSection("Game Function")
+local a = w:NewTab("Game")
+local b = a:NewSection("Auto Farm")
 
 
-Section1:UpdateSection("Ultimate GUI")
 
-Section1:NewToggle("Auto Click", "Makes the game to Click for you!", function(state)
+b:NewToggle("Auto Click", "Makes the game to Click for you!", function(state)
     if state then
         --[[game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.SwordService.Swing:FireServer()]]print("Boom")
     else
@@ -18,7 +15,7 @@ Section1:NewToggle("Auto Click", "Makes the game to Click for you!", function(st
     end
 end)
 
-Section1:NewToggle("Auto Rebirth", "Makes the game to Auto Rebirth for you!", function(state)
+b:NewToggle("Auto Rebirth", "Makes the game to Auto Rebirth for you!", function(state)
     if state then
         --[[game.GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.Rebirth:InvokeServer(unpack(args))]]print("Boom")
     else
@@ -26,7 +23,7 @@ Section1:NewToggle("Auto Rebirth", "Makes the game to Auto Rebirth for you!", fu
     end
 end)
 
-Section1:NewToggle("Auto Prestige", "Makes the game to Auto Prestige for you!", function(state)
+b:NewToggle("Auto Prestige", "Makes the game to Auto Prestige for you!", function(state)
     if state then
         --[[game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.PrestigeService.Prestige:InvokeServer()]]print("Boom")
     else
