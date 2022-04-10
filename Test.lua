a2b8bd3ed3aae30ae9d5f4fb21ab942658b4e1ef
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-10 15:16:06 +0800

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,8 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Test", "Synapse")
+local Tab = Window:NewTab("Test Name")
+local Section = Tab:NewSection("Test Section")
+Section:NewButton("ButtonTest", "XXX", function()
+    print("Clicked")
+end)
