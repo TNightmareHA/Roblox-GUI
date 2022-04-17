2a6fe6e7453bfd10ede2f9bc7c89bac0e57fa68f
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-17 11:12:19 +0800

    Update and rename Test.lua to Fick.lua

--- Fick.lua
+++ Fick.lua
@@ -1,5 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/main/Source.lua"))()
-
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local w = library.CreateLib("Specter_Hub", "Synapse")
 local t = w:NewTab("Maps")
 local s = t:NewSection("Art Gallery")
