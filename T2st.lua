0874165c1f85ef1f5b28737b82a54212b463f63f
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-17 11:06:05 +0800

    Update T2st.lua

--- T2st.lua
+++ T2st.lua
@@ -1,9 +1,9 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/main/Source.lua"))()
 
 local w = lib.CreateLib("Specter Hub", "Synapse")
 local t = w:NewTab("Maps")
 local s = t:NewSection("Art Gallery")
-s:NewLabel("Playing on Art Gallery")
+
 s:NewButton("Art Gallery", "Click this if you're playing on AG", function()
     print("Underconstruction")
 end)
