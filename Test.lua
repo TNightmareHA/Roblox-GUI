894743bfd05ff1b0229d7f4bd023eac0cb28c5f0
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-17 11:10:13 +0800

    Update and rename TEST.lua to Test.lua

---
+++ Test.lua
@@ -0,0 +1,5 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/main/Source.lua"))()
+
+local w = library.CreateLib("Specter_Hub", "Synapse")
+local t = w:NewTab("Maps")
+local s = t:NewSection("Art Gallery")
