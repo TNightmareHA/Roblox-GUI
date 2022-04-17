bb002234be230f7de6269af62510c329383463ca
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-17 10:58:18 +0800

    Update and rename TeSt.lua to Test.lua

---
+++ Test.lua
@@ -0,0 +1,13 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/main/Source.lua"))()
+
+local w = lib.CreateLib("Specter Hub", "Synapse")
+local t = w:NewTab("Maps")
+local s = t:NewSection("Art Gallery")
+s:NewLabel("Playing on Art Gallery")
+s:NewButton("Art Gallery", "Click this if you're playing on AG", function()
+    print("Underconstruction")
+end)
+
+s:NewButton("Chalet", "Click this if you're playing on Chalet", function()
+    print("Underconstruction")
+end)
