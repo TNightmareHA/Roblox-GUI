6a7057c40087696eb5f95e5805faea242b44399d
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2022-04-17 12:05:30 +0800

    Update and rename Test.lua to Good.lua

--- Good.lua
+++ Good.lua
@@ -1,6 +1,6 @@
 -- init
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/main/Code.lua"))()
-local venyx = library.new("Venyx", 5013109572)
+local venyx = library.new("Venyx",)
 
 -- themes
 local themes = {
